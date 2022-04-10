c0b568cf0356e08c32e6fe70185c466cc598b0f1
Shubham Kumar <54644253+shubham7668@users.noreply.github.com>
2022-04-10 16:33:52 +0530

    Add files via upload

---
+++ attendance.sql
@@ -0,0 +1,33 @@
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+CREATE DATABASE IF NOT EXISTS `attendance` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
+USE `attendance`;
+
+
+CREATE TABLE IF NOT EXISTS `objects` (
+  `uid` int(11) NOT NULL AUTO_INCREMENT,
+  `teacher_uid` int(11) NOT NULL,
+  `code` varchar(50) NOT NULL,
+  `year` varchar(10) NOT NULL,
+  `section` varchar(50) NOT NULL,
+  `object` longblob NOT NULL,
+  PRIMARY KEY (`uid`),
+  UNIQUE KEY `uid` (`uid`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+
+
+
+CREATE TABLE IF NOT EXISTS `teacher` (
+  `uid` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(25) NOT NULL,
+  `email` varchar(25) NOT NULL,
+  `phone` varchar(25) NOT NULL,
+  `password` varchar(100) NOT NULL,
+  PRIMARY KEY (`uid`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
